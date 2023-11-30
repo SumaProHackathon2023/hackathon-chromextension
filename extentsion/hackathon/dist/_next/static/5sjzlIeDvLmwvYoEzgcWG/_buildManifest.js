f6996b8fab731efefee2c5268d42ed80989f1f74
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2023-11-30 16:58:27 +0900

    これでコミットします

---
+++ extentsion/hackathon/dist/_next/static/5sjzlIeDvLmwvYoEzgcWG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9449d0475a400e8d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- extentsion/next.config.js
+++ extentsion/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    distDir: "dist",
+    distDir: "hackathon/dist",
     reactStrictMode: true,
     images: {
         unoptimized: true
