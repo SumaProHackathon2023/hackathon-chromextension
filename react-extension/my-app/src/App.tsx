dac1bfcc4250d7e1ff2d8cdacd91ac638f5b9170
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2023-12-01 17:34:04 +0900

    これはいったん保存するしかないで

---
+++ react-extension/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import EventList from './FetchFirestore';
+
+function App() {
+  
+  return (
+      <main className="App">
+          <EventList />
+      </main>
+  );
+}
+
+export default App;
