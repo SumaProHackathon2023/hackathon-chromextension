50b10899d21d88f3b6d8beb359cba6121a801348
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2023-12-02 17:36:25 +0900

    Update firebase-init.ts

--- webpack_compile_mode/src/firebase-init.ts
+++ webpack_compile_mode/src/firebase-init.ts
@@ -1,6 +1,8 @@
 import {initializeApp} from "firebase/app"
 import {getFirestore} from "firebase/firestore"
-
+/**
+* これはわかりきった話だが、環境変数直置きはよろしくないので修正しよう。
+*/
 const firebaseConfig = {
     apiKey: "AIzaSyDbre0EtcsZvohXl6pP2uuc5eC-5nE1kpo",
     authDomain: "annoyingadvertisements-63b44.firebaseapp.com",
